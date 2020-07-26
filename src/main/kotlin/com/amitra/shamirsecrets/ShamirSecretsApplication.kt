3e4266a44eea071caa5d8dc949f99d8987549255
arnabmitra <amitra@figure.com>
2020-07-26 16:35:34 -0600

    more stuff

---
+++ src/main/kotlin/com/amitra/shamirsecrets/ShamirSecretsApplication.kt
@@ -0,0 +1,7 @@
+package com.amitra.shamirsecrets
+
+
+
+fun main(args: Array<String>) {
+	println("hello scooby")
+}
---
+++ src/test/kotlin/com/amitra/shamirsecrets/ShamirSecretsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.amitra.shamirsecrets
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShamirSecretsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
